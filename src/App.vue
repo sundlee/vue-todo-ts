811defa98bf58bfb6e11869be83c210f35677c56
sundlee <sundlee@gmail.com>
2022-03-09 17:22:30 +0900

    initial commit + 인풋 컴포넌트의 emit 이벤트 정의 및 구현

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Vue Todo with Typescript!</h1>
+    <TodoInput :item="todoText" @input="updateTodoText" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TodoInput from "./components/TodoInput.vue";
+
+export default Vue.extend({
+  components: { TodoInput },
+  data() {
+    return {
+      todoText: "hello world!",
+    };
+  },
+  methods: {
+    addTodo() {
+      console.log("add");
+    },
+    updateTodoText(value: any) {
+      this.todoText = value;
+    },
+  },
+});
+</script>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" :value="item" @input="handleInput" />
+    <button @click="addTodo" type="button">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["item"],
+  methods: {
+    handleInput(event: any) {
+      this.$emit("input", event.target.value);
+    },
+    addTodo() {
+      console.log("add");
+    },
+  },
+});
+</script>
