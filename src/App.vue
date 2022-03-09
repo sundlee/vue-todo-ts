218e924386cc93ffe0e60d0a925959fc81ce9a27
sundlee <sundlee@gmail.com>
2022-03-09 20:45:28 +0900

    할 일 아이템 목록 컴포넌트 설계 및 구현

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <h1>Vue Todo with Typescript!</h1>
-    <TodoInput :item="todoText" @input="updateTodoText" @add="addTodoItem" />
+    <header>
+      <h1>Vue Todo with Typescript!</h1>
+    </header>
+    <main>
+      <TodoInput :item="todoText" @input="updateTodoText" @add="addTodoItem" />
+      <div>
+        <ul>
+          <todo-list-item></todo-list-item>
+          <!-- <li>아이템 1</li> -->
+          <!-- <li>아이템 2</li> -->
+          <!-- <li>아이템 3</li> -->
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TodoInput from "./components/TodoInput.vue";
+import TodoListItem from "./components/TodoListItem.vue";
 
 export default Vue.extend({
-  components: { TodoInput },
+  components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "hello",
